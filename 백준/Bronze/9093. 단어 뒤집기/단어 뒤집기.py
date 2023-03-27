65b24cd81026f0cda1c4bac83ade43e029545d28
김희윤 <99259398+heeyoonjik@users.noreply.github.com>
2023-03-27 14:39:27 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 768 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,14 +1,5 @@
-import sys
-input = sys.stdin.readline
-
 for _ in range(int(input())):
-  inputSentence = input().split()
-  for i in inputSentence:
-    characterSet = list(i)
-    stack = []
-    for k in characterSet:
-      stack.append(k)
-    originalStackLength = len(stack)
-    for _ in range(originalStackLength):
-      print(stack.pop(), end="")
-    print(end=" ")
+  word = input().split()
+  for w in word:
+    print(w[::-1], end=" ")
+  print()
